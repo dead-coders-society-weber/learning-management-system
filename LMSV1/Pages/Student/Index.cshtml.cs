19dfff7dc16a9720aaf09f4d6d23d26d73972985
Zac Morgan <zacharymorgan3@mail.weber.edu>
2023-09-24 08:01:28 -0600

    Student folder
    Created Student folder and scaffolding for course model

---
+++ LMSV1/Pages/Student/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LMSV1.Models;
+
+namespace LMSV1.Pages.Student
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LMSV1CourseContext _context;
+
+        public IndexModel(LMSV1CourseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Course != null)
+            {
+                Course = await _context.Course.ToListAsync();
+            }
+        }
+    }
+}
