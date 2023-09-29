438a3d3d72879ec595200125a946fde31544ac80
Tenzoh <quinnblackford@mail.weber.edu>
2023-09-28 22:20:14 -0600

    Link to account page/ Page creation
    Link if student is signed in that takes the student to the account page added to the top menu. Account page added but essentially empty for now.

---
+++ LMSV1/Pages/Student/Account.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMSV1.Pages.Student
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
